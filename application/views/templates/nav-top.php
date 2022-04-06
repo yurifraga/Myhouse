537d9bfc05d5dd9813d09029aa8f7ec98eae165b
Yuri Fraga <yuri.fraga@hotmail.com>
2022-04-06 13:06:14 -0300

    -Adicionando função para o botão "deslogar" no topo da pagina

--- application/views/templates/nav-top.php
+++ application/views/templates/nav-top.php
@@ -7,7 +7,7 @@
 	</div>
   <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
-      <a class="nav-link" href="">Sign out</a>
+      <a class="nav-link" href="<?= base_url() ?>login/logout">Sign out</a>
     </li>
   </ul>
 </nav>
